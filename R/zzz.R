dbe38beed83a07923a9b3b3e5ec9e4e53401dcef
Thomas Dybdal Pedersen <thomasp85@gmail.com>
2014-11-21 08:56:09 +0100

    Registration of localMax and ionFilter methods in prepareMethods

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,7 @@
 .onLoad <- function(libname, pkgname) {
+    # Spectrum processing registration
+    prepareMethods$registerMethod('localMax', localMax, list(mode='profile'))
+    prepareMethods$registerMethod('ionFilter', ionFilter, list(mode='centroid'))
     # Peak detection registration
     peakMethods$registerMethod('massifquant', massifquant, list(mode='centroid'))
 }
